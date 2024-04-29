1116d1fc76ab201455a42487d975618310695d55
Matias Torres <mtri21019@gmail.com>
2024-04-29 17:42:56 -0300

    super queryes

---
+++ superQuerys.sql
@@ -0,0 +1,22 @@
+-- Se puede poner condiciones
+select count(book_id), sum(if(cond, 1, false)) from books
+
+select count(book_id), sum(if(year < 1950, 1, 0)) as '<1950' from books;
+
+select count(book_id),
+sum(if(year < 1950, 1, 0)) as '<1950',
+sum(if(year >= 1950 and year < 1990,1,0)) as '<1990',
+sum(if(year >= 1990 and year < 2000,1,0)) as '<2000',
+sum(if(year >= 2000 ,1,0)) as '<hoy'
+from books;
+
+select nationality, count(book_id),
+sum(if(year < 1950, 1, 0)) as '<1950',
+sum(if(year >= 1950 and year < 1990,1,0)) as '<1990',
+sum(if(year >= 1990 and year < 2000,1,0)) as '<2000',
+sum(if(year >= 2000 ,1,0)) as '<hoy'
+from books as b
+join authors as a
+ on a.author_id = b.author_id
+ where a.nationality is not null
+ GROUP BY nationality;
